3e8cedc423a2e165c992543717b00a136da6ac26
Maya Shaked <mayaromyshaked@yahoo.com>
2024-12-05 15:03:21 +0100

    prepend 0 to folder names

--- advent-of-code-2024/day05/solution.py
+++ advent-of-code-2024/day05/solution.py
@@ -5,22 +5,23 @@ def part1():
     updates = load_data(filename='updates.txt', delimiter=",")
 
     faulty_updates = []
-    for rule_pair in rules:
+    for rule_pair in rules: # get rules
         to_print_before, to_print_after = rule_pair
-        for i, update in enumerate(updates):
-            if to_print_before in update and to_print_after in update:
-                if update.index(to_print_before) > update.index(to_print_after):
+        for i, update in enumerate(updates): # go through each update
+            if to_print_before in update and to_print_after in update: # check whether either rule value is in update
+                if update.index(to_print_before) > update.index(to_print_after): # check for incorrect order
                     if i not in faulty_updates:
-                        faulty_updates.append(i)
+                        faulty_updates.append(i) # save index of faulty updates
 
     correct_updates_middle_sum = 0
-
     for i, update in enumerate(updates):
-        if i not in faulty_updates:
-            correct_updates_middle_sum += int(update[len(update)//2])
+        if i not in faulty_updates: # we only want the correctly-ordered updates
+            correct_updates_middle_sum += update[len(update)//2] # get middle value in update list
 
     print(correct_updates_middle_sum)
     return(correct_updates_middle_sum)
+
+
 def load_data(filename, delimiter):
     data = []
     with open(filename, 'r') as file:
