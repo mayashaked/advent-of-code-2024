1b639352b602a25905e85be646aa0343d52e7ccc
Maya Shaked <mayaromyshaked@yahoo.com>
2024-12-05 14:20:31 +0100

    day 5 part 1

---
+++ advent-of-code-2024/day5/solution.py
@@ -0,0 +1,33 @@
+
+def part1():
+
+    rules = load_data(filename='page_orders.txt', delimiter="|")
+    updates = load_data(filename='updates.txt', delimiter=",")
+
+    faulty_updates = []
+    for rule_pair in rules:
+        to_print_before, to_print_after = rule_pair
+        for i, update in enumerate(updates):
+            if to_print_before in update and to_print_after in update:
+                if update.index(to_print_before) > update.index(to_print_after):
+                    if i not in faulty_updates:
+                        faulty_updates.append(i)
+
+    correct_updates_middle_sum = 0
+
+    for i, update in enumerate(updates):
+        if i not in faulty_updates:
+            correct_updates_middle_sum += int(update[len(update)//2])
+
+    print(correct_updates_middle_sum)
+    return(correct_updates_middle_sum)
+def load_data(filename, delimiter):
+    data = []
+    with open(filename, 'r') as file:
+        for row in file:
+            str_list = row.strip("\n").split(delimiter)
+            data.append([int(x) for x in str_list])
+    return(data)
+
+if __name__ == "__main__":
+    part1()
